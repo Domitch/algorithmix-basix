247065b15a6dc0f2f07e491ac092f34551ea220b
Domintch <d191190@hotmail.com>
2026-06-19 11:20:20 +0200

    first homeword hw-input

---
+++ hw-input/run.js
@@ -0,0 +1,10 @@
+// Do not change, helper to read arguments:
+const args = process.argv.slice(2);
+
+// Implement below:
+let name = args[0]
+let output = "Hello World! Your name is" +" " + name;
+
+
+// Do not change:
+console.log(output);
